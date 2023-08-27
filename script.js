11a8c68de6ef7c560e500cbf69b146441a3d4b97
Rizuan <john.lee.coco@gmail.com>
2023-08-27 21:07:08 +0800

    Change pixel colour on mousedown & hover

--- script.js
+++ script.js
@@ -16,6 +16,24 @@ function createBoard(length) {
         board.appendChild(row);
     }
     
+    const pixels = document.querySelectorAll(".pixel");
+    pixels.forEach((pixel) => {
+        pixel.addEventListener("mousedown", (event) => {
+            event.target.style.backgroundColor = "#000000";
+        });
+
+        // When the cursor hovers over a box, check if the left mouse button is being depressed.
+        // If it is being depressed, change the colour accordingly.
+        // More info here: https://stackoverflow.com/questions/15098584/check-if-mouse-button-is-down-while-hovering
+
+        pixel.addEventListener("mouseover", (event) => {
+            if (event.buttons == 1) {
+                console.log(event.target);
+                event.target.style.backgroundColor = "#000000";
+            }
+        });
+    });
+
     // Container will stay same size - the width/height of each div can be calculated (width of container / no. of boxes).
     // Add an event listener to each ^ div that listens for mousedown, and changes colour accordingly.
 
