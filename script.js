6db482a3a941cba0d444532f48daee26ebc241be
Rizuan <john.lee.coco@gmail.com>
2023-09-02 13:47:48 +0800

    Add button to erase & reset board

--- script.js
+++ script.js
@@ -42,15 +42,28 @@ function clearBoard() {
 }
 
 function initialiseSettings() {
-    // https://stackoverflow.com/questions/63410174/running-a-function-while-changing-range-slider
-    
+    const eraseButton = document.querySelector("#erase-button");
     const boardSizeInput = document.querySelector("#board-size-slider");
     const boardSizeLabel = document.querySelector(".board-size-label");
 
+    // Initialise button to erase board.
+    
+    eraseButton.addEventListener("click", () => {
+        clearBoard();
+        createBoard(+boardSizeInput.getAttribute("value"));
+    });
+    
+    // Initialise size selection slider.
+    // https://stackoverflow.com/questions/63410174/running-a-function-while-changing-range-slider
+    
     boardSizeInput.addEventListener("input", (event) => {
         boardSizeLabel.textContent = `${event.target.value} x ${event.target.value}`;
         clearBoard();
         createBoard(event.target.value);
+    });
+
+    boardSizeInput.addEventListener("change", (event) => {
+        boardSizeInput.setAttribute("value", +event.target.value);
     });
 }
 
